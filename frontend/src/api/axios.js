99a3584fef27c678b6f5d777c8135913b6e69fae
Omar <nyirurugoomar@gmail.com>
2025-07-29 17:48:15 +0200

    Update axios url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://connectly-app-oapq.onrender.com/api',
 });
 
 // Add request interceptor to include auth token
