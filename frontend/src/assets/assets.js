3026fd444d84303cdd43646da90cfb16c328eca6
Omar <nyirurugoomar@gmail.com>
2025-07-29 10:25:14 +0200

    change photo name

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -2,7 +2,7 @@ import logo from './logo.png'
 import userProfile from './userProfile.png'
 import cleaner from './cleaner.png'
 import Delivery from './Delivery.png'
-import Lawn from './lawn.png'
+import Lawn from './Lawn.png'
 
 export const assets = {
     logo,
