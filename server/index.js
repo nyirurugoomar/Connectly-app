50b3c50c5a59ebd5617b67f487c831a3c2cf3f56
Omar <nyirurugoomar@gmail.com>
2025-07-29 11:12:14 +0200

    update cors url for frontend

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin:['http://localhost:5173'],
+    origin:['http://localhost:5173','https://connectly-application.netlify.app/'],
     credentials:true,
 }));
 
